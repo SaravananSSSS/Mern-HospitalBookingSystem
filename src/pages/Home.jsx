e5742087e466f0e1ca489e921d58af793d09ee48
saravananssss <saravanan.k.selvakumar@gmail.com>
2024-04-25 09:47:55 +0530

    faq section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,14 @@ import { BsArrowRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import About from '../components/About/About';
 import featureImg from '../assets/images/feature-img.png';
+import faqImg from "../assets/images/faq-img.png";
 import ServiceList from '../components/Services/ServiceList';
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
+import DoctorList from '../components/Doctors/DoctorList';
+import FaqList from '../components/Faq/FaqList';
+
+
 
 
 
@@ -214,7 +219,39 @@ const Home = () => {
         </div>
       </section>
       {/*---- feature section end-----*/}
-     
+      {/*----our great doctors------*/}
+      <section>
+        <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+          <h2 className="heading text-center">Our Great Doctors</h2>
+          <p className="text__para text-center">
+            world-class care for everyone. Our health System offers unmatched,
+            expert health care.
+          </p>
+        </div>
+        <DoctorList/>
+        </div>
+      </section>
+      {/*----our great doctors------*/}
+
+      {/*----faq section starts----*/}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="" />
+            </div>
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most Questions by our Beloved patients
+              </h2>
+
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/*----faq section ends----*/}
     </>
   );
 };
